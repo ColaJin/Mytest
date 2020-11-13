90e7c46830c37ead1434d4ea4b5c2b2aab899749
jinyandong <jinyandong@jsjd.cc>
2020-11-13 10:54:45 +0800

    167. 两数之和 II - 输入有序数组方法更新

--- blankProgram/src/main/java/leetcode/TwoSumII.java
+++ blankProgram/src/main/java/leetcode/TwoSumII.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
  *
@@ -21,10 +24,16 @@ public class TwoSumII {
         int target = 9;
         int[] rs = twoSum(numbers, target);
         int[] ints = twoSumByTwoPoint(numbers, target);
+        int[] anoth = twoSumByDichotomy(numbers, target);
+        int[] last = twoSumByHashSheet(numbers, target);
+
         for (int i = 0; i < rs.length; i++) {
 //            System.out.println(rs[i]);
-            System.out.println(ints[i]);
+//            System.out.println(anoth[i]);
+            System.out.println(last[i]);
+//            System.out.println(ints[i]);
         }
+
     }
 
     //暴力求解
@@ -66,7 +75,52 @@ public class TwoSumII {
 
     //二分查找法
     //可以固定一个数，然后利用二分查找法，在该数的右侧查找
-    //TODO
+    public static int[] twoSumByDichotomy(int[] numbers, int target) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            int hight = numbers.length - 1;
+            //控制low
+            int low = i + 1;
+            while (low <= hight) {
+                int mid = (hight - low) / 2 + low;
+                //计算的是numbers[i]不是number[low]
+                if (numbers[i] + numbers[mid] == target) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[i] + numbers[mid] > target) {
+
+                    hight = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+
+
+    }
 
     //使用Hash表,存储当前结点的值和当前结点的坐标+1(从1开始计算位置),计算target-当前是否存在Hash表，返回坐标+1的值
+
+    public static int[] twoSumByHashSheet(int[] numbers, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.isEmpty()) {
+                map.put(numbers[i], i + 1);
+            } else {
+                int temp = target - numbers[i];
+                //map的get方法时抛出空指针异常，换成containsKey方法，或者使用!map.get(temp).equals(null)方法
+                // 不可以使用map.get(temp) != null
+                if (map.containsKey(temp)) {
+                    return new int[]{map.get(target - numbers[i]), i + 1};
+                }else {
+                    map.put(numbers[i],i+1);
+                }
+            }
+        }
+        return new int[]{-1, -1};
+
+
+    }
 }
