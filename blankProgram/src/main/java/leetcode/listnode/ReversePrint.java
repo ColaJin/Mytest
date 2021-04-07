6a0166944e96b09c94a3ceafaa7c991b66acefa6
jinyandong <jinyandong@jsjd.cc>
2021-04-07 10:05:56 +0800

    内容调整

--- blankProgram/src/main/java/leetcode/listnode/ReversePrint.java
+++ blankProgram/src/main/java/leetcode/listnode/ReversePrint.java
@@ -15,6 +15,22 @@ public class ReversePrint {
             System.out.println(i);
         }
     }
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
     public static int[] reversePrint(ListNode head) {
 
